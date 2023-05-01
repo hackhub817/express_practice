4c31bdabe8e4c772ddb041049cab579f09fac7bc
hackhub817 <piyushguptaji123@gmail.com>
2023-05-01 14:56:21 +0530

    first

--- server.js
+++ server.js
@@ -7,18 +7,34 @@ const app=express();
 app.set('view engine','ejs');
 /// VIEWS FOLDER BY DEFAULT VIEWS FOLDER MAIN HI FILES KO SEARCH KARAT AH
 // AGAR HAME KOI FOLDER USE KARNA HT TO HAM 
-// app.set('views':'templates');
+// app.set('views':'templates')
 
 console.log(app.get('views'));
+
 app.use(express.static('public'));
 
+// AISE HAM TAB LIKHEGAY JAB  HAM EJS KA USE NAHI KAR RAHE HOGAY
+
+// app.get("/",function(req,res)
+// {
+//     res.sendFile(path.join(__dirname,'/index.html'));
+// });
+// app.get("/about",function(req,res)
+// {
+//     res.sendFile(path.join(__dirname,'/about.html'));
+// });
+
+
+// AFTER INSTALLING AJS TEMOLATE
 app.get("/",function(req,res)
 {
-    res.sendFile(path.join(__dirname,'/index.html'));
+    res.render("index",{
+        Title:"elllo"
+    });
 });
 app.get("/about",function(req,res)
 {
-    res.sendFile(path.join(__dirname,'/about.html'));
+    res.render("about");
 });
 
 app.listen(3000,()=>
